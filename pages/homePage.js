785f8f63d727a5f9a4a31d5373bd67c0f7c53542
Satyendra Verma <satyendra.verma@91trucks.com>
2024-09-30 16:09:30 +0530

    Second commint

--- pages/homePage.js
+++ pages/homePage.js
@@ -10,7 +10,6 @@ class HomePage {
     this.busesButton = this.page.getByRole('button', { name: 'Buses' }).first();
     this.autoRickshawsButton = this.page.getByRole('button', { name: 'Auto Rickshaws' }).first();
     this.searchButton =  this.page.locator("div[class='block'] button[name='Search']");
-    //this.page.getByRole('button', { name: 'Search' });
     this.searchCombobox = this.page.getByRole('combobox');
   }
 
@@ -62,23 +61,3 @@ module.exports = HomePage;
 
 
 
-
-// import { test, expect } from '@playwright/test';
-
-// test('test', async ({ page }) => {
-//   await page.goto('https://www.91trucks.com/');
-//   await page.getByLabel('Home').click();
-//   await page.getByRole('heading', { name: 'What are you looking for?' }).click();
-//   await page.getByRole('link', { name: 'subcategory Pickup' }).click();
-//   await page.goto('https://www.91trucks.com/');
-//   await page.getByRole('link', { name: 'subcategory Mini' }).click();
-//   await page.goto('https://www.91trucks.com/');
-//   await page.getByRole('link', { name: 'subcategory Three Wheeler' }).click();
-//   await page.getByRole('link', { name: 'subcategory Bus' }).click();
-//   await page.getByRole('heading', { name: 'Most Popular Trucks' }).click();
-//   await page.getByRole('heading', { name: 'Trucks Dealers in Popular' }).click();
-//   await page.getByRole('heading', { name: 'Most Popular Buses' }).click();
-//   await page.getByRole('heading', { name: 'Most Popular Auto Rickshaws' }).click();
-//   await page.getByRole('heading', { name: 'Most Popular Comparison' }).click();
-//   await page.getByRole('heading', { name: 'Latest News' }).click();
-// });
