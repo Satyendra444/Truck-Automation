29069a80c1c2df0588ee74cfb12fda5532e58fd1
Satyendra Verma <vermasatyendra77@gmail.com>
2024-09-30 16:47:13 +0530

    befor all

--- pages/homePage.js
+++ pages/homePage.js
@@ -6,10 +6,7 @@ class HomePage {
     this.page = page;
     this.homeLink = this.page.getByLabel('Home');
     this.heading = this.page.getByRole('heading', { name: 'Select Brand' });
-    this.trucksButton = this.page.getByRole('button', { name: 'Trucks' }).first();
-    this.busesButton = this.page.getByRole('button', { name: 'Buses' }).first();
-    this.autoRickshawsButton = this.page.getByRole('button', { name: 'Auto Rickshaws' }).first();
-    this.searchButton =  this.page.locator("div[class='block'] button[name='Search']");
+    this.searchButton = this.page.locator("div[class='block'] button[name='Search']");
     this.searchCombobox = this.page.getByRole('combobox');
   }
 
@@ -18,8 +15,7 @@ class HomePage {
     await this.homeLink.click();
   }
 
-  async verifyTitle() {
-    const expectedTitle = "New Trucks, Three Wheelers & Buses, Prices, Latest Commercial Vehicle News in India";
+  async verifyTitle(expectedTitle) {
     await expect(this.page).toHaveTitle(expectedTitle);
   }
 
@@ -27,37 +23,22 @@ class HomePage {
     await expect(this.heading).toBeVisible();
   }
 
-  async verifyButtonsPresence() {
-    await expect(this.trucksButton).toBeVisible();
-    await expect(this.busesButton).toBeVisible();
-    await expect(this.autoRickshawsButton).toBeVisible();
+  async verifyButtonPresence(buttonName) {
+    const button = this.page.getByRole('button', { name: buttonName }).first();
+    await expect(button).toBeVisible();
   }
 
-  async selectTruckBrand(brand) {
-    await this.trucksButton.click();
+  async selectBrand(brand) {
     await this.searchCombobox.selectOption(brand);
-  }
-
-  async verifySearchBoxPresence() {
-    await expect(this.searchCombobox).toBeVisible();
-  }
-
-  async verifySearchButtonPresence() {
-    await expect(this.searchButton).toBeVisible();
   }
 
   async clickSearchButton() {
     await this.searchButton.click();
   }
 
-  async verifyBestTataTrucksHeading() {
-    const bestTataTrucksHeading = this.page.getByRole('heading', { name: 'Best Tata Trucks in India' });
-    await expect(bestTataTrucksHeading).toBeVisible();
+  async verifyElementVisible(locator) {
+    await expect(locator).toBeVisible();
   }
 }
 
 module.exports = HomePage;
-
-
-
-
--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -1,44 +1,49 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const HomePage = require('../pages/homePage');
+
+test.beforeEach(async ({ page }) => {
+  const homePage = new HomePage(page);
+  await homePage.navigateToHome();
+});
 
 test('Verify the Home page title', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.verifyTitle();
+  const expectedTitle = "New Trucks, Three Wheelers & Buses, Prices, Latest Commercial Vehicle News in India";
+  await homePage.verifyTitle(expectedTitle);
 });
+
 test('Verify heading of Select Brand options', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
   await homePage.verifyHeading();
-  
 });
+
 test('Verify the presence of Trucks, Buses, and Auto Rickshaws buttons', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.verifyButtonsPresence();
+  const buttons = ['Trucks', 'Buses', 'Auto Rickshaws'];
+  for (const button of buttons) {
+    await homePage.verifyButtonPresence(button);
+  }
 });
-test('Verify the brand serach box present or not ', async ({ page }) => {
+
+test('Verify the brand search box is present', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.verifySearchBoxPresence();
-  
+  await homePage.verifyElementVisible(homePage.searchCombobox);
 });
+
 test('Verify the select brand flow', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.selectTruckBrand('tata');
+  await homePage.selectBrand('tata');
 });
+
 test('Verify search Button CTA', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.verifySearchButtonPresence();
-
+  await homePage.verifyElementVisible(homePage.searchButton);
 });
 
 test('Verify Tata Trucks search flow', async ({ page }) => {
   const homePage = new HomePage(page);
-  await homePage.navigateToHome();
-  await homePage.selectTruckBrand('tata');
+  await homePage.selectBrand('tata');
   await homePage.clickSearchButton();
-  await homePage.verifyBestTataTrucksHeading();
+  const bestTataTrucksHeading = page.getByRole('heading', { name: 'Best Tata Trucks in India' });
+  await homePage.verifyElementVisible(bestTataTrucksHeading);
 });
