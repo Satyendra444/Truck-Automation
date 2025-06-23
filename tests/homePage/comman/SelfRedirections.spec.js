12957397e29add283fcd376601858248d0a1ec35
Satyendra Verma <vermasatyendra77@gmail.com>
2025-06-23 17:09:39 +0530

    self redirection added

---
+++ tests/homePage/comman/SelfRedirections.spec.js
@@ -0,0 +1,55 @@
+const { test, expect } = require('@playwright/test');
+const urls = require('../../../config/url');
+
+const paths = [
+  '/tractors',
+  '/compare-tractor',
+  '/tractor-dealers',
+  '/tractor-servicecenters',
+  '/tractor-spareparts',
+  '/tractors-spareparts',
+  '/tractors-dealers',
+  '/tractor-bodymakers',
+  '/en/tractors',
+  '/en/compare-tractor',
+  '/en/tractor-dealers',
+  '/en/tractor-servicecenters',
+  '/en/tractor-spareparts',
+  '/en/tractors-spareparts',
+  '/en/tractors-dealers',
+  '/en/tractor-bodymakers',
+  '/hi/tractors',
+  '/hi/compare-tractor',
+  '/hi/tractor-dealers',
+  '/hi/tractor-servicecenters',
+  '/hi/tractor-spareparts',
+  '/hi/tractors-spareparts',
+  '/hi/tractors-dealers',
+  '/hi/tractor-bodymakers',
+  '/construction-equipments',
+  '/compare-construction-equipments',
+  '/construction-equipments-servicecenters',
+  '/construction-equipments-spareparts',
+  '/construction-equipments-dealers',
+  '/construction-equipments-bodymakers',
+  '/en/construction-equipments',
+  '/en/compare-construction-equipments',
+  '/en/construction-equipments-servicecenters',
+  '/en/construction-equipments-spareparts',
+  '/en/construction-equipments-dealers',
+  '/en/construction-equipments-bodymakers',
+  '/hi/construction-equipments',
+  '/hi/compare-construction-equipments',
+  '/hi/construction-equipments-servicecenters',
+  '/hi/construction-equipments-spareparts',
+  '/hi/construction-equipments-dealers',
+  '/hi/construction-equipments-bodymakers',
+];
+
+paths.forEach((path) => {
+  test(`Verify title and redirection for ${path}`, async ({ page }) => {
+    const baseUrl = urls.base;
+    await page.goto(`${baseUrl}${path}`);
+    await expect(page).toHaveTitle('New Truck, Bus and 3 Wheelers: Commercial Vehicles In India - 91trucks');
+  });
+});
